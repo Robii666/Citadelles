d049fdc2d81b6a28c122bc6a02b42a0f280c8d3f
robin.milon <robin.milon@free.fr>
2023-11-06 09:08:29 +0100

    Dépot du fichier src

---
+++ src/main/java/modele/Quartier.java
@@ -0,0 +1,12 @@
+package modele;
+
+public class Quartier {
+    // Attributs privés
+    private nom;
+    private type;
+    private int coutConstruction;
+    private String caractéristiques;
+
+    // La constante TYPE QUARTIERS
+    public static final TYPE_QUARTERS ="";
+}
